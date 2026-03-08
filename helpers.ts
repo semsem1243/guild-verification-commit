4c2350c4f9d7f07652e24b6f80016e150242c68f
semsem1243 <154025468+semsem1243@users.noreply.github.com>
2026-03-08 15:36:24 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T12:36:08.493Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-08T12:36:23.323Z
+Commit: Fix race condition in async operations
