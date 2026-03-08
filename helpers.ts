386e8a74e32744fdbe477a7c915b84111ccec449
semsem1243 <154025468+semsem1243@users.noreply.github.com>
2026-03-08 15:37:15 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T12:37:10.656Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-08T12:37:13.781Z
+Commit: Fix memory leak in event handlers
