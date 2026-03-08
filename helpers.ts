16a802e5ad18dd1f31fe9bd2f0a02f41aeb7cabb
semsem1243 <154025468+semsem1243@users.noreply.github.com>
2026-03-08 15:36:54 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T12:36:49.934Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-08T12:36:53.396Z
+Commit: Patch security vulnerability in authentication
