b930a2161118774e0cda782141bf5eb51e007cc1
semsem1243 <154025468+semsem1243@users.noreply.github.com>
2026-03-08 15:36:09 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T12:35:59.733Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-08T12:36:08.493Z
+Commit: Patch API endpoint error handling
