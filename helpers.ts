74994e10389598f34e00ea6acb398dfa26fbedf6
semsem1243 <154025468+semsem1243@users.noreply.github.com>
2026-03-08 15:35:17 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T12:34:53.610Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-08T12:35:16.210Z
+Commit: Resolve null pointer exception in user service
