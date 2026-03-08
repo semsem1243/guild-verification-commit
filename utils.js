4621249f8bdb5d47a8373b448c4d0da2ba00cf8a
semsem1243 <154025468+semsem1243@users.noreply.github.com>
2026-03-08 15:36:15 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T12:36:04.295Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-08T12:36:14.024Z
+Commit: Patch API endpoint error handling
